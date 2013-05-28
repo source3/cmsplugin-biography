8a6699f02b1843970df94e27adf23ef5295651ee
Kevin Richardson <kevin@magically.us>
2013-05-28 13:30:47 -0400

    Add information to PersonBiography admin page
    The following fields were added to the admin:
    * last_name
    * first_name
    * title
    * employer
    * active
    
    Furthermore, `active` was added to the admin page's list_filter

--- cmsplugin_biography/admin.py
+++ cmsplugin_biography/admin.py
@@ -4,6 +4,8 @@ from .models import PersonBiography
 
 
 class PersonBiographyAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'title', 'employer', 'active', )
+    list_filter = ('active', )
     search_fields = ('^last_name', '^first_name', )
 
 admin.site.register(PersonBiography, PersonBiographyAdmin)
