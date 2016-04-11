8357e732a42e81449b4e70a9d96cb531c35bde20
Andrew Parker <andrew@source3.com>
2016-04-11 18:24:57 +0000

    Add migration to updated cmsplugin table name for DjangoCMS 3.x

---
+++ migrations/0002_rename_cmsplugin.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        db.rename_table('cmsplugin_personbiographypluginmodel', 'cmsplugin_biography_personbiographypluginmodel')
+
+    def backwards(self, orm):
+        db.rename_table('cmsplugin_biography_personbiographypluginmodel', 'cmsplugin_personbiographypluginmodel')
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'cmsplugin_biography.personbiography': {
+            'Meta': {'ordering': "('last_name', 'first_name')", 'object_name': 'PersonBiography'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'description': ('djangocms_text_ckeditor.fields.HTMLField', [], {}),
+            'employer': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'suffix': ('django.db.models.fields.CharField', [], {'max_length': '40', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
+        },
+        u'cmsplugin_biography.personbiographypluginmodel': {
+            'Meta': {'ordering': "('person',)", 'object_name': 'PersonBiographyPluginModel', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'event_description': ('djangocms_text_ckeditor.fields.HTMLField', [], {'blank': 'True'}),
+            'person': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cmsplugin_biography.PersonBiography']"}),
+            'short_description': ('djangocms_text_ckeditor.fields.HTMLField', [], {'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['cmsplugin_biography']
